a59cd63edbebb97c30af9d58d529227fcbfb7046
Volodymyr Volianyk <wrspct@gmail.com>
2025-08-03 16:55:34 +0300

    worked with lesson 1 js

---
+++ lessons/lesson-1-2/scripts/main.js
@@ -0,0 +1,19 @@
+console.log("1")
+{
+    var a = "hello";
+    let b = "world";
+}
+console.log(a);
+//console.log(b);
+
+const PI = 3.14;
+console.log(PI);
+
+let c = "hello okten";
+console.log(typeof c, c);
+
+let d = 100500;
+console.log(typeof d, d);
+
+let e = true;
+console.log(typeof e, e);
