fd54d27116083e1f8c890334e0907f7a0b8cb583
Volodymyr Volianyk <wrspct@gmail.com>
2026-02-03 22:02:23 +0200

    Finish working with hw 7.

--- homeWork/lesson7/hw7-6/scripts/main.js
+++ homeWork/lesson7/hw7-6/scripts/main.js
@@ -1,2 +1,29 @@
 //#gsKLAsNWM
 // *Через Array.prototype. створити власний foreach, filter
+
+const nums = [1, 2, 3, 4, 5];
+
+Array.prototype.myForEach = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+        callback(this[i], i, this);
+    }
+};
+
+nums.myForEach((value, index) => {
+    console.log(value, index);
+});
+
+Array.prototype.myFilter = function (callback) {
+    const result = [];
+
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+            result.push(this[i]);
+        }
+    }
+
+    return result;
+};
+
+const evens = nums.myFilter(n => n % 2 === 0);
+console.log(evens);
