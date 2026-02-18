bc35e7df1b0a67aa5a64b7d5d4c65532a5c7c748
Volodymyr Volianyk <wrspct@gmail.com>
2026-02-18 22:37:09 +0200

    Finish working with hw 8.

--- homeWork/lesson8/hw8-2/scripts/main.js
+++ homeWork/lesson8/hw8-2/scripts/main.js
@@ -3,3 +3,31 @@
 // Додати перевірки на undefined, null, NaN.
 // Подумати і реалізувати логіку, в якій кінцевий об’єкт буде мати функції, які в нього були до цього моменту.
 
+function cloner(obj) {
+    if (obj) {
+        let functions = [];
+
+        for (const key in obj) {
+            if (typeof obj[key] === 'function') {
+                const functionClone = obj[key].bind({});
+                functions.push({functionClone, key});
+            }
+        }
+
+        const cloneObj = JSON.parse(JSON.stringify(obj));
+        for (const func of functions) {
+            cloneObj[func.key] = func.functionClone;
+        }
+
+        console.log(cloneObj);
+        return cloneObj;
+
+    }
+    throw new Error('Object is undefined, null or NaN.');
+}
+
+cloner({
+    id: 1, name: "John", age: 20, greeting() {
+    }, call() {
+    }
+});
