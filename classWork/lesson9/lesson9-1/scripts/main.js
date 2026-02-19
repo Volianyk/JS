a04de3ac02e0c97de017a4ee6d400fe81ef61ffc
Volodymyr Volianyk <wrspct@gmail.com>
2026-02-19 23:16:32 +0200

    Start working with lesson 9.

---
+++ classWork/lesson9/lesson9-1/scripts/main.js
@@ -0,0 +1,18 @@
+console.log(document.head);
+console.log(document.head.innerHTML);
+console.log(document.head.children);
+console.log(document.body.innerHTML);
+
+//ID
+console.log(document.getElementById('list1'));
+
+//class
+console.log(document.getElementsByClassName('menu'));
+
+//TAG
+console.log(document.getElementsByTagName('ul'));
+
+//query selector
+
+console.log(document.querySelector('ul'));
+console.log(document.querySelectorAll('ul'));
